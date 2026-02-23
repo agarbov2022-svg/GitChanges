0ee1b19a118dd3c1e49d10b9bd6caf64716d9222
agarbov2022-svg <a.garbov2022@pmg-pernik.com>
2026-02-23 08:39:06 +0200

    a

---
+++ bbbb/bbbb/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace bbbb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string command = Console.ReadLine();
+                if(command == "Reverse")
+                {
+                    Array.Reverse(a);
+                }
+                else if(command == "Distinct")
+                {
+                    string[] unique = a.Distinct();
+                    string[] a = unique;
+                }
+                else if(command == "Replace")
+                {
+                    int index = int.Parse(Console.ReadLine());
+                    string rep = Console.ReadLine();
+                    a[index] = $"{rep}";
+                }
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
